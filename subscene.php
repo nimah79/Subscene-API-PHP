c5922707984cf6a8709afc5b15a5d0443f774289
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-03-27 12:15:11 +0000

    Apply fixes from StyleCI

--- subscene.php
+++ subscene.php
@@ -7,22 +7,21 @@ http://nimatv.ir
 */
 
 header('Content-Type: application/json');
-if(isset($_GET["movie"])) {
-  $search = file_get_contents("https://subscene.com/subtitles/title?q=".urlencode($_GET["movie"])."&l=farsi_persian");
-  if(preg_match('/<div class="title">.*?<a href="(.*?)">.*?<\/a>.*?<\/div>/s', $search, $title)) {
-    $list = file_get_contents("https://subscene.com".$title[1]."/farsi_persian");
-    if(preg_match('/<td class="a1">.*?<a href="(.*?)">.*?<span class="l r positive-icon">/s', $list, $sub)) {
-      $page = file_get_contents("https://subscene.com".$sub[1]);
-      if(preg_match('/<div class="download">.*?<a href="(.*?)" rel="nofollow" onclick="DownloadSubtitle\(this\)" id="downloadButton" class="button positive">.*?Download Farsi/s', $page, $download)) {
-        preg_match('/<title>(.*?)<\/title>/', str_replace("Subscene - Subtitles for ", "", $list), $name);
-        preg_match('/<img src="(.*?)"/', $list, $poster);
-        exit(json_encode(["title" => htmlspecialchars_decode(str_replace("&#39;", "'", $name[1])), "poster" => $poster[1], "download" => "https://subscene.com".$download[1]]));
-      }
-      exit('{"error":"not found"}');
+if (isset($_GET['movie'])) {
+    $search = file_get_contents('https://subscene.com/subtitles/title?q='.urlencode($_GET['movie']).'&l=farsi_persian');
+    if (preg_match('/<div class="title">.*?<a href="(.*?)">.*?<\/a>.*?<\/div>/s', $search, $title)) {
+        $list = file_get_contents('https://subscene.com'.$title[1].'/farsi_persian');
+        if (preg_match('/<td class="a1">.*?<a href="(.*?)">.*?<span class="l r positive-icon">/s', $list, $sub)) {
+            $page = file_get_contents('https://subscene.com'.$sub[1]);
+            if (preg_match('/<div class="download">.*?<a href="(.*?)" rel="nofollow" onclick="DownloadSubtitle\(this\)" id="downloadButton" class="button positive">.*?Download Farsi/s', $page, $download)) {
+                preg_match('/<title>(.*?)<\/title>/', str_replace('Subscene - Subtitles for ', '', $list), $name);
+                preg_match('/<img src="(.*?)"/', $list, $poster);
+                exit(json_encode(['title' => htmlspecialchars_decode(str_replace('&#39;', "'", $name[1])), 'poster' => $poster[1], 'download' => 'https://subscene.com'.$download[1]]));
+            }
+            exit('{"error":"not found"}');
+        }
+        exit('{"error":"not found"}');
     }
     exit('{"error":"not found"}');
-  }
-  exit('{"error":"not found"}');
 }
 exit('{"error":"parameter movie is required"}');
-?>
