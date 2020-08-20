5583425dfd965945c51a2b13dafc97697086eacd
Nima HeydariNasab <nima.heydari79@yahoo.com>
2020-08-20 15:28:56 +0430

    Remove "clean" method from tests

--- tests/SubsceneTest.php
+++ tests/SubsceneTest.php
@@ -4,21 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 final class SubsceneTest extends TestCase
 {
-    protected $cookie_file_path = __DIR__.'/../cookies.txt';
-
-    protected function clean()
-    {
-        if (file_exists($this->cookie_file_path)) {
-            unlink($this->cookie_file_path);
-        }
-    }
-
     public function testSearch()
     {
         require_once __DIR__.'/../Subscene.php';
         $subscene = new Subscene();
         $this->assertNotEmpty($subscene->search('Fast Five'));
-        $this->clean();
     }
 
     public function testGetSubtitles()
@@ -26,7 +16,6 @@ final class SubsceneTest extends TestCase
         require_once __DIR__.'/../Subscene.php';
         $subscene = new Subscene();
         $this->assertNotEmpty($subscene->getSubtitles('https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist'));
-        $this->clean();
     }
 
     public function testGetSubtitleInfo()
@@ -34,6 +23,5 @@ final class SubsceneTest extends TestCase
         require_once __DIR__.'/../Subscene.php';
         $subscene = new Subscene();
         $this->assertNotEmpty($subscene->getSubtitleInfo('https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist/farsi_persian/1108695'));
-        $this->clean();
     }
 }
