e6e26eb585896ab724e04f5a7f9068bfa9639d6d
Nima HeydariNasab <nimah79@users.noreply.github.com>
2020-08-20 13:11:56 +0000

    Apply fixes from StyleCI

--- tests/SubsceneTest.php
+++ tests/SubsceneTest.php
@@ -6,25 +6,25 @@ final class SubsceneTest extends TestCase
 {
     public static function setUpBeforeClass(): void
     {
-        require_once __DIR__ . "/../Subscene.php";
+        require_once __DIR__.'/../Subscene.php';
     }
 
     public function testSearch()
     {
-        $this->assertNotEmpty(Subscene::search("Fast Five"));
+        $this->assertNotEmpty(Subscene::search('Fast Five'));
     }
 
     public function testGetSubtitles()
     {
-        $this->assertNotEmpty(Subscene::getSubtitles("https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist"));
+        $this->assertNotEmpty(Subscene::getSubtitles('https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist'));
     }
 
     public function testGetSubtitlesCustomLanguage()
     {
-        $subtitles = Subscene::getSubtitles("https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist", [46]);
+        $subtitles = Subscene::getSubtitles('https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist', [46]);
         $invalid_language_found = false;
-        foreach ($subtitles["subtitles"] as $subtitle) {
-            if ($subtitle["language"] != "Farsi/Persian") {
+        foreach ($subtitles['subtitles'] as $subtitle) {
+            if ($subtitle['language'] != 'Farsi/Persian') {
                 $invalid_language_found = true;
                 break;
             }
@@ -34,6 +34,6 @@ final class SubsceneTest extends TestCase
 
     public function testGetSubtitleInfo()
     {
-        $this->assertNotEmpty(Subscene::getSubtitleInfo("https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist/farsi_persian/1108695"));
+        $this->assertNotEmpty(Subscene::getSubtitleInfo('https://subscene.com/subtitles/fast-five-fast-and-furious-5-the-rio-heist/farsi_persian/1108695'));
     }
 }
